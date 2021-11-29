41e169bc55d781f5908984f48af315ff9a2fc645
prabhakarans10 <95216438+prabhakarans10@users.noreply.github.com>
2021-11-29 17:45:19 +0530

    Update App.java
    final

--- DemoProject/src/main/java/com/abc/App.java
+++ DemoProject/src/main/java/com/abc/App.java
@@ -8,7 +8,7 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello Wo" );
+        System.out.println( "Hello World!" );
         System.out.println( "TRIGGER!" );
         System.out.println( "TRIGGER222!" );
     }
